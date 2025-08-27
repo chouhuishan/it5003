ad6ad1946f66b7f666050987bb1e3f5417d362af
Chou Hui Shan <huishaan@live.com>
2025-08-27 20:00:14 +0800

    kattis\leetcode taught in class : sort of sorting and merge string alternatively

--- Class Coding/merge_string_alternatively.py
+++ Class Coding/merge_string_alternatively.py
@@ -1,13 +1,15 @@
 def merge_alternatively(word1: str, word2: str) -> str:
     i, j, n, m = 0, 0, 0, 0
     k = 0
-    ans = ""
-    while 1 < n and j < m:
+    ans = []
+    while 1 < n and j < m:  # this is O(n+m)**2
         if k % 2 == 0:  # take from word1
-            ans += word1[i]
+            # ans += word1[i] -> this cannot be used; see comments below
+            ans.append(word1[i])
             i += 1
         else:  # take from word2
-            ans += word2[i]
+            # ans += word2[i] -> this cannot be used
+            ans.append(word1[i])
             j += 1
         k += 1
 
@@ -15,3 +17,7 @@ def merge_alternatively(word1: str, word2: str) -> str:
         ans += word2[j]
         j += 1
     return ans  # O(10 + n+m + n + m + 1) ~~O(n+n)
+
+
+# PROBLEM : Ans is a string, therefore, it is timebomb , because it is immutable
+# SOLUTION: Use a list of string
